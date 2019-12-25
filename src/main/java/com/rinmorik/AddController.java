eb4c27407e7abf6ed454d5095e67c1eb189a1848
= <=>
2019-12-25 14:23:58 +0530

    version 2 with service layer and display.jsp

--- src/main/java/com/rinmorik/AddController.java
+++ src/main/java/com/rinmorik/AddController.java
@@ -1,15 +1,34 @@
 package com.rinmorik;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mysql.jdbc.PreparedStatement.ParseInfo;
+import com.rinmorik.services.AddService;
 
 
 @Controller
 public class AddController {
 	
 	@RequestMapping("/add")
-	public void add(){
-		System.out.println("Hiiii");
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response){
+		
+		int i = Integer.parseInt(request.getParameter("t1"));
+		int j = Integer.parseInt(request.getParameter("t2"));
+		
+		AddService as = new AddService();
+		int k = as.add(i, j);
+		 
+		//System.out.println("Hiiii");
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Display.jsp");
+		mv.addObject("result",k);
+		return mv;
 	}
 
 }
